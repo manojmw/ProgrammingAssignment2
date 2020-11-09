bd7c2ac5d20384610ed0ef31de12ab28fa73dcc1
manojmw <74168582+manojmw@users.noreply.github.com>
2020-11-09 12:11:44 +0530

    Add files via upload
    Assignment: Caching the Inverse of a Matrix - Programming assignment - Week 3 - Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following functions will create a special vector. 
+# If inverse is already present, it will get the data from the cache or else it will find the inverse an store it in cache.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The first function is used to set a value to the matrix
+makeCache_Matrix <- function(x = matrix()) {
+            z <- NULL
+            set <- function(y) {
+              x <<- y
+              z <<- NULL
+              }
+            
+## This function is used to get the value of the matrix         
+get <- function() x
+## This function is used to set the value of the inverse
+setinverse <- function(inverse) z <<- inverse
+## This function is used to get the value of the inverse
+getinverse <- function() z
+list(set = set, get = get,
+                 setinverse = setinverse,
+                 getinverse = getinverse)
+            
 }
 
 
-## Write a short comment describing this function
-
+## The following function will get the inverse of the special character
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      z <- x$getinverse()
+      if(!is.null(z)) {
+          message("Please wait...Getting the cached data")
+          return(z)
+  }
+  data <- x$get()
+  z <- solve(data, ...)
+  x$setinverse(z)
+  z
 }
+
+#Testing the above functions : 
+Mat <- matrix(c(3,4,5,6),2,2)
+M <- makeCache_Matrix(Mat)
+cacheSolve(M) # This cacheSolve will actually provide us the inverse from the cache
